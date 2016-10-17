3e27eb9a9aae0539261f08e5ff646657483057fb
mark <h091237557@gmail.com>
2016-10-17 22:31:47 +0800

    Add:add delete data unit test

--- server/model/data/data-facade.js
+++ server/model/data/data-facade.js
@@ -1,32 +1,47 @@
 //var dataSchema = require('./data-schema');
 
 class DataModel {
-	constructor(Schema){
-		this.Schema = Schema;
-	}
+  constructor(Schema) {
+    this.Schema = Schema;
+  }
 
-	create(input) {
-		const schema = new this.Schema(input);
-		let promise = new Promise((resolve,reject) => {
-			schema.save((err) => {
-				if(err){
-					reject(err);
-				}else{
-					resolve({"status":"Success"});
-				}
-			});
-		});
-		return promise;
-	}
+  create(input) {
+    const schema = new this.Schema(input);
+    let promise = new Promise((resolve, reject) => {
+      schema.save((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({
+            "status": "Success"
+          });
+        }
+      });
+    });
+    return promise;
+  }
 
-	find(query) {
-		var promise = new Promise((resolve,reject) => {
-			this.Schema.find(query,(err,data) => {
-				resolve(data);
-			});
-		})
-		return promise;
-	}
+  find(query) {
+    var promise = new Promise((resolve, reject) => {
+      this.Schema.find(query, (err, data) => {
+        resolve(data);
+      });
+    })
+    return promise;
+  }
+
+  remove(id) {
+    var promise = new Promise((resolve, reject) => {
+      this.Schema.findByIdAndRemove(id, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    return promise;
+  }
 }
 
 module.exports = DataModel;
--- server/test/data/data-facade-test.js
+++ server/test/data/data-facade-test.js
@@ -26,7 +26,7 @@ describe('UNIT:data-facade.js -- Get All Data', function() {
       "test": "mark"
     };
     var query = "";
-		var Facade = new DataFacade(Schema);
+    var Facade = new DataFacade(Schema);
     Schema.find.yields(null, expectResult);
     var result = Facade.find(query);
     result.then((datas) => {
@@ -38,53 +38,103 @@ describe('UNIT:data-facade.js -- Get All Data', function() {
 });
 
 describe('UNIT:data-facade.js -- Save Data', () => {
-	var save;
-	beforeEach(() => {
-		//let schema = new Schema(mockDatas);
-		//save = sinon.stub(schema,'save')
-	});
+  var Facade;
+  beforeEach(() => {});
 
-	before(() => {
-		sinon.stub(Schema.prototype,'save');
-	});
+  before(() => {
+    Facade = new DataFacade(Schema);
+    sinon.stub(Schema.prototype, 'save');
+  });
 
-	afterEach(() => {
-		Schema.prototype.save.reset();
-	});
+  afterEach(() => {
+    Schema.prototype.save.reset();
+  });
 
-	after(() => {
-		Schema.prototype.save.restore();
-	});
+  after(() => {
+    Schema.prototype.save.restore();
+  });
 
-	it('should save success',(done) => {
-		let obj = JSON.stringify(mockDatas);
-		let datas = {
-			"id" : "1",
-			"data" : obj,
-			"describe" : "test",
-			"author" : "Mark"
-		}
-		Schema.prototype.save.yields(null,{"status" : "success"});		
-		let Facade = new DataFacade(Schema);
-		let result = Facade.create(datas);
-		result.then((msg) => {
-			expect(msg).to.exist;
-			done();
-		}).catch((err) => {
-			done();
-		});
-	});
+  it('should save success', (done) => {
+    let obj = JSON.stringify(mockDatas);
+    let datas = {
+      "id": "1",
+      "data": obj,
+      "describe": "test",
+      "author": "Mark"
+    }
+    Schema.prototype.save.yields(null, {
+      "status": "success"
+    });
+    let Facade = new DataFacade(Schema);
+    let result = Facade.create(datas);
+    result.then((msg) => {
+      expect(msg).to.exist;
+      expect(Schema.prototype.save.callCount).to.equal(1);
+      done();
+    }).catch((err) => {
+      done();
+    });
+  });
 
-	it('should save error due to vaild error',(done) => {
-		let Facade = new DataFacade(Schema);
-		Schema.prototype.save.yields({"status":"faild"},null);
-		var result = Facade.create(mockDatas);
-		result.then((msg) => {
-			var aa = err;
-		}).catch((err) => {
-			expect(err).to.exist;
-			done();
-		});
-	});
+  it('should save error due to vaild error', (done) => {
+    Schema.prototype.save.yields({
+      "status": "faild"
+    }, null);
+    let result = Facade.create(mockDatas);
+    result.then((msg) => {}).catch((err) => {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
+});
+
+describe('UNIT:data-facade.js -- Delete data', function() {
+  var Facade;
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+
+  });
+
+  before(() => {
+    Facade = new DataFacade(Schema);
+    sinon.stub(Schema, 'findByIdAndRemove');
+  });
+
+  after(() => {
+
+  });
+  it('should delete success', (done) => {
+    var expectedResult = {
+      status: true
+    };
+    Schema.findByIdAndRemove.yields(null, expectedResult);
+    var result = Facade.remove("1");
+    result.then((msg) => {
+      expect(msg).to.exist;
+      done();
+    }).catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+
+  it('should delete fail due to no find id', (done) => {
+    var expectedResult = {
+      status: false
+    };
+    Schema.findByIdAndRemove.yields(expectedResult, null);
+    var result = Facade.remove('1');
+    result.then((msg) => {
+
+    }).catch((err) => {
+      expect(err).to.exist;
+      done();
+    });
+
+  });
 
 });
