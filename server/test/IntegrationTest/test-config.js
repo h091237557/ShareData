fd886f959b2b0730231d77dc8f329ebc4800b8a3
mark <h091237557@gmail.com>
2016-11-25 22:26:09 +0800

    Server-Fix : fix test connect db

--- server/test/IntegrationTest/test-config.js
+++ server/test/IntegrationTest/test-config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-exports.DB = 'mongodb://localhost/test';
+exports.DB = 'mongodb://localhost/test1';
 
 exports.connect = (callback) => {
 	mongoose.connect(exports.DB,callback);
